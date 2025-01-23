d6053a4fcb3a40f5aea1b842193d8ebe9769af88
Jothanf <jothanferney@gmail.com>
2025-01-23 14:48:15 -0500

    Enhance settings configuration for REST framework and JWT authentication
    - Added REST_FRAMEWORK settings to configure JWT authentication.
    - Defined SIMPLE_JWT settings to specify access and refresh token lifetimes.
    - Included necessary imports for timedelta to manage token expiration.
    
    This update improves security and session management for API interactions.

--- pmback/settings.py
+++ pmback/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -176,3 +177,14 @@ CORS_ALLOW_ALL_ORIGINS = True  # Solo para desarrollo
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
