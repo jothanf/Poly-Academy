f181d5c1e84e410b66c5830e932e0254b70cd13f
Lizeth castillo <64451121+lizethcas@users.noreply.github.com>
2025-01-27 19:03:22 -0500

    nueco commit

--- pmback/settings.py
+++ pmback/settings.py
@@ -138,10 +138,33 @@ MEDIA_URL = '/media/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
     "http://127.0.0.1:8000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # Asegúrate de que la carpeta 'media' exista
 if not os.path.exists(MEDIA_ROOT):
